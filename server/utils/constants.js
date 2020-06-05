5f8869d825ed23372ed300772dea3e1f4b2df29f
leonardo <leon.ak@hotmail.com>
2020-06-05 01:22:03 -0300

    add leaderboard constants

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,12 +1,18 @@
 module.exports = {
-  messages: {
-    error: {
-      UNEXPECTED_RUNNING: 'An unexpected error occurred while processing your request.',
-    },
+  endpoints: {
+    LEADER_BOARD: '/',
   },
   error: {
     name: {
       VALIDATION_ERROR: 'ValidationError',
     },
   },
+  messages: {
+    error: {
+      UNEXPECTED_RUNNING: 'An unexpected error occurred while processing your request.',
+    },
+  },
+  tables: {
+    LEADER_BOARD: 'LeaderBoard',
+  }
 };
