0cea65662c6632da6dd6d3306226a9ff1f420d75
leonardo <leon.ak@hotmail.com>
2020-06-05 01:11:07 -0300

    add leaderboard route

---
+++ server/core/routers/v1/leaderboard.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const constants = require('../../../utils/constants');
+const retrieveControllers = require('../../../utils/retrieveControllers');
+const retrieveSchemas = require('../../../utils/retrieveSchemas');
+
+const router = express.Router();
+
+const controllers = retrieveControllers(
+  __filename.split(/\\routers|\/routers/)[1].split('.')[0]
+);
+const schemas = retrieveSchemas(__filename.split(/\\routers|\/routers/)[1].split('.')[0]);
+
+module.exports = router;
